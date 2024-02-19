0fc9a0418287bc64590500cfca781107c49dcf5e
Kooreis <kooreis.official@gmail.com>
2024-02-19 01:57:12 -0600

    In the first update, the user is prompted to enter a number. This number is then converted to an Excel-style column name using the 'NumberToColumnName' method.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a full working console application in C# that solves the problem:
-
-```C#
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,34 +9,4 @@ class Program
         Console.WriteLine("Enter a number to convert to Excel-style column name:");
         int number = int.Parse(Console.ReadLine());
         Console.WriteLine("Excel-style column name: " + NumberToColumnName(number));
-
-        Console.WriteLine("Enter an Excel-style column name to convert to number:");
-        string columnName = Console.ReadLine();
-        Console.WriteLine("Number: " + ColumnNameToNumber(columnName));
     }
-
-    static string NumberToColumnName(int number)
-    {
-        var columnName = new Stack<char>();
-        while (number > 0)
-        {
-            columnName.Push((char)('A' + --number % 26));
-            number /= 26;
-        }
-        return new string(columnName.ToArray());
-    }
-
-    static int ColumnNameToNumber(string columnName)
-    {
-        int number = 0;
-        for (int i = 0; i < columnName.Length; i++)
-        {
-            number *= 26;
-            number += columnName[i] - 'A' + 1;
-        }
-        return number;
-    }
-}
-```
-
-This console application first asks the user to enter a number, which it then converts to an Excel-style column name using the `NumberToColumnName` method. It then asks the user to enter an Excel-style column name, which it converts to a number using the `ColumnNameToNumber` method.
